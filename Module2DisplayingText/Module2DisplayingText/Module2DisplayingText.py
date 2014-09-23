873bd138d458c5b1e967a69269afee1c5bc96c33
hockeygeekgirl <susan.ibach@live.com>
2014-09-23 10:35:50 -0700

    Code examples from module 2

---
+++ Module2DisplayingText/Module2DisplayingText/Module2DisplayingText.py
@@ -0,0 +1,17 @@
+print('The capybara is the worlds largest rodent')
+print('The capybara likes to live in groups')
+print("The capybara can swim")
+
+print("the capybara lives in \nSouth America")
+print("""This is the strangest
+way to print over multiple lines I know""")
+
+print('here is a double quote "' + " here is a single quote '")
+print("or you can just do this \" does that work")
+print("can I just print a \ on the screen?")
+
+#I am inserting a \\ so the \ appears correctly in the string
+print("But what if I want \\news ")
+
+
+
