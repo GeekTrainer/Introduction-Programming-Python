6ff797bc1114f265d65c5c28db100a5d689c7ec9
hockeygeekgirl <susan.ibach@live.com>
2014-09-23 15:50:25 -0700

    Module 6 if statements

--- Module6MakingDecisionsWithCode/Module6MakingDecisionsWithCode/Module6MakingDecisionsWithCode.py
+++ Module6MakingDecisionsWithCode/Module6MakingDecisionsWithCode/Module6MakingDecisionsWithCode.py
@@ -1,1 +1,21 @@
-print('Hello World')
+#meanwhile earlier in the day
+bestTeam = "senators"
+
+#if statements with strings
+#favouriteTeam = input("What is your favourite hockey team? ")
+#if favouriteTeam.upper() == bestTeam.upper() :
+#    print("Yeah Go Sens Go")
+#    print("But I miss Alfredsson")
+#print ("It's okay if you prefer football/soccer")
+
+#if with numbers
+freeToaster = None 
+
+deposit = int(input("how much do you want to deposit "))
+if deposit > 100 :
+    freeToaster = True
+
+#complex code here...
+if freeToaster :
+    print("enjoy your toaster")
+print("Have a nice day!")
