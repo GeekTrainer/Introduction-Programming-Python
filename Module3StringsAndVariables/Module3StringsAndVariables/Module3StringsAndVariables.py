546220fab500bf46872d81cc7daa2c0a5073799c
GeekTrainer <chrhar@microsoft.com>
2014-09-23 11:46:32 -0700

    Module 3

---
+++ Module3StringsAndVariables/Module3StringsAndVariables/Module3StringsAndVariables.py
@@ -0,0 +1,15 @@
+#Collect name from the user
+name = input('What is your name? ')
+country = input('What country do you live in? ')
+country = country.upper()
+
+#Display the name
+#print(name)
+
+#Create a friendly output
+print('\nHello, ' + name + '. You live in ' + country)
+
+
+#Update the value
+#name = 'Christopher Harrison'
+#print(name)
