05560043d7c9481c5ae2137f5eb592c937f77e87
GeekTrainer <chrhar@microsoft.com>
2014-09-23 14:39:31 -0700

    Dates and Times

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,19 @@
+import datetime
+
+currentDate = datetime.datetime.today()
+#print(currentDate.minute)
+#print(currentDate)
+#print(currentDate.month)
+#print(currentDate.year)
+
+#print(currentDate.strftime('%d %b, %Y'))
+#print(currentDate.strftime('%d %b %y'))
+
+
+
+#userInput = input('Please enter your birthday (mm/dd/yyyy)')
+#birthday = datetime.datetime.strptime(userInput, '%m/%d/%Y').date()
+#print(birthday)
+
+#days = birthday - currentDate
+#print(days.days)
