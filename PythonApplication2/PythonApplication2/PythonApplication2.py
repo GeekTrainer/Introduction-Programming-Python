8ce6e9a28c74d9240c5468a22b5189d275934136
hockeygeekgirl <susan.ibach@live.com>
2014-09-24 09:56:04 -0700

    for loops

--- PythonApplication2/PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,1 +1,19 @@
+import turtle
+#nbrsides = 5
+#for weirdname in range(nbrsides) :
+#    turtle.forward(100)
+#    turtle.right(360/nbrsides)
+#    for steps in range(nbrsides) :
+#        turtle.forward(50) 
+#        turtle.right(360/nbrsides)
+
+for colour in ['red','green','blue','black'] :
+    turtle.color(colour)
+    turtle.forward(100)
+    turtle.left(90)
+
+
+
+
+
 
