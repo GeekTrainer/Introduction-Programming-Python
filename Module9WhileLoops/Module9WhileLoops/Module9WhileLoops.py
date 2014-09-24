039a2ea506cb50544911682ceff83219d9c73cc1
GeekTrainer <chrhar@microsoft.com>
2014-09-24 10:41:09 -0700

    Slides and module

---
+++ Module9WhileLoops/Module9WhileLoops/Module9WhileLoops.py
@@ -0,0 +1,15 @@
+import turtle
+
+counter = 0
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter += 1 # This is the exact same thing as counter = counter + 1
+
+
+#answer = '0'
+
+#while answer != '42':
+#    answer = input('What is the answer to the ultimate question of life, the universe and everything? ')
+
+#print('Congratulations - you are right!!')
